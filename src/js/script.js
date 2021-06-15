281445cb91914adadcc38d62019b53f83e979326
Michał Młynarczyk <michalmlyn@gmail.com>
2021-06-15 14:42:21 +0200

    ceny2

--- src/js/script.js
+++ src/js/script.js
@@ -169,7 +169,7 @@
           // check if there is param with a name of paramId in formData and if it includes optionId //
           if(formData[paramId] && formData[paramId].includes(optionId)) {
             // check if the option is not default //
-            if(option.default) {
+            if(!option.default) {
               // add option price to price variable //
               price = price + option.price;
               console.log('option.price: ', option.price);
